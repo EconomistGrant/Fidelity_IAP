f09ae9925540ceabf5bf54b52bbee996c9b463bb
songhao_mit_win <songhao@mit.edu>
2021-01-14 21:06:17 -0500

    dynamic cppi and no negative equity holdings

--- dynamic_CPPI.py
+++ dynamic_CPPI.py
@@ -6,44 +6,42 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import numpy as np
 import matplotlib.pyplot as plt
 from strategy import PortfolioStrategy
-class CPPI(PortfolioStrategy):
-    """Implementing vanilla CPPI strategy
-    
-    The investor have a target floor value that grows with risk-free rate
-    Start with 1, at any time, he will determine the floor value of next period by "guessing" the risk-free rate 
-    of next period, and then determine cushion and amount of equity shares accordingly
-        
+class DynamicCPPI(PortfolioStrategy):
+    """Implementing dynamic CPPI strategy
+
+    Compared with vanilla CPPI strategy, the floor value will not increase at the risk-free rate, but 
+    rather determined based on a fixed proportion of the maximum portfolio value ever reached        
     Attributes:
     -----------
         equity_returns: a numpy array of risky returns
         bond_returns:   a numpy array of risk-free rates
-        floor:          initial floor as a proportion of initial portfolio value
+        floor:          floor as a proportion of maximum portfolio value ever reached
         multiple:       proportion of cushion held in equity
-
+        
         floor_ts:       np.array of floor values growing with risk-free rate
         equity_holdings:np.array of equity value hold 
         bond_holdings:  np.array of bond value hold
-        nav:            np.array of total portfolio value
-    """
+        nav:            np.array of total portfolio value"""
     def __init__(self,
                  equity_returns:np.array,
                  bond_returns: np.array,
-                 initial_floor: float,
+                 floor: float,
                  multiple: float):
-        self.initial_floor = initial_floor
+        self.floor = floor
         self.multiple = multiple
         self.bond_returns = bond_returns
         self.equity_returns = equity_returns
-
+        
         assert len(equity_returns) == len(bond_returns), 'Inputs should have the same dimension'
         self.num_periods = len(equity_returns)
         self.floor_ts = np.zeros(self.num_periods)
         self.bond_holdings = np.zeros(self.num_periods)
         self.equity_holdings = np.zeros(self.num_periods)
         self.nav = np.ones(self.num_periods)
-
-        self.floor_ts[0] = self.initial_floor
         
+        self.floor_ts[0] = self.floor
+        self.max_nav = 1
+
         for t in range(1,self.num_periods):
             # Set positions for time t at the end of time t-1
             prev_nav = self.nav[t-1]
@@ -58,21 +56,21 @@ class CPPI(PortfolioStrategy):
             
             self.equity_holdings[t] = equity_holding
             self.bond_holdings[t] = bond_holding
-            
-            # Realized values at the end of time t but before setting positions for time t + 1
-            realized_floor = self.floor_ts[t-1] * (1+self.bond_returns[t])
-            self.floor_ts[t] = realized_floor
 
             self.nav[t] = self.nav[t-1] + bond_holding*bond_returns[t] + equity_holding*equity_returns[t]
             
+            if self.nav[t] > self.max_nav:
+                self.max_nav = self.nav[t]
+            self.floor_ts[t] = self.max_nav * floor            
 
 if __name__ == '__main__':
-    simulated_bond_returns = np.array([2,3,4,3,2,1,2,3,4,3])/100
-    simulated_equity_returns = np.array([9,-2,8,-1,7,-3,6,-2,5,0])/100
+    simulated_bond_returns = np.array([-2,-3,-4,-3,-2,-1,-2,-3,-4,-3])/100
+    simulated_equity_returns = np.array([9,-2,-8,-1,-7,-3,-6,-2,-5,0])/100
     multiple = 0.5
-    initial_floor = 0.8
+    floor = 0.8
 
-    cppi = CPPI(simulated_equity_returns,simulated_bond_returns,initial_floor,multiple)
-    cppi.plot()
-    cppi.plot('bond_and_equity')
-    cppi.plot('floor_and_cushion')
+    dcppi = DynamicCPPI(simulated_equity_returns,simulated_bond_returns,floor,multiple)
+    dcppi.plot()
+    dcppi.plot('bond_and_equity')
+    dcppi.plot('floor_and_cushion')
+    print(dcppi.equity_holdings)
